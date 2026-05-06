dc9568dfbc50a3b1b3ae53e9508b6a9e30286050
Takahiro Kaneko <tkaneko188@gmail.com>
2026-05-06 16:12:41 +0900

    Ignore generated asset manifest

--- src/lib/asset-manifest.generated.ts
+++
@@ -1,7 +0,0 @@
-export const assetManifest = {
-  'app.css': '/app.6fe93953.css',
-  'markdown-editor.mjs': '/markdown-editor.353e810b.mjs',
-} as const
-
-export type AssetName = keyof typeof assetManifest
-
