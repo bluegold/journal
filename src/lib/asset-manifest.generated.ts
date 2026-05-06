bf52cd0fc52440a8c502daf9ce8d86126d85b814
Takahiro Kaneko <tkaneko188@gmail.com>
2026-05-06 16:14:45 +0900

    Track generated asset manifest

---
+++ src/lib/asset-manifest.generated.ts
@@ -0,0 +1,7 @@
+export const assetManifest = {
+  'app.css': '/app.84b13f81.css',
+  'markdown-editor.mjs': '/markdown-editor.353e810b.mjs',
+} as const
+
+export type AssetName = keyof typeof assetManifest
+
